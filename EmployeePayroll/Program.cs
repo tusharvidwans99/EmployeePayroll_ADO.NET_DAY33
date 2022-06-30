1bf89bdf663918b53c3490ad9edb70ff78bfb689
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-07-01 01:31:04 +0530

    Adding Welcome Message

---
+++ EmployeePayroll/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace EmployeePayroll
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Employee Payroll Program on which we are doing operation on SQL data using C#");
+
+            //EmployeeRepository employeeRepository = new EmployeeRepository();
+            //employeeRepository.GetAllemployee();
+
+        }
+    }
+}
