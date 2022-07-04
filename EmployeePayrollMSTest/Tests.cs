849a75b96a7ea63518b49f7a17748ec221d4bd77
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-07-05 00:37:48 +0530

    [Tushar Vidwans] Adding MStest for updating the salary

--- EmployeePayrollMSTest/Tests.cs
+++ EmployeePayrollMSTest/Tests.cs
@@ -8,13 +8,19 @@ namespace EmployeePayrollMSTest
         [TestMethod]
         public void GivenSalaryDetails_AbleToUpdateSalaryDetails()
         {
+
+            //Arrange
             EmployeeRepository repository = new EmployeeRepository();
             EmployeeModel model = new EmployeeModel();
             model.EmployeeID = 1;
             model.EmployeeName = "Vishal";
             model.BasicPay = 2000;
+
+            //Act
             repository.UpdatingSalaryInDataBase(model);
             decimal actual = repository.ReadingUpdatedSalaryFromDataBase();
+
+            //Assert
             Assert.AreEqual(model.BasicPay, actual);
         }
 
